bc9645f1c82664010a2a1923e7c45f62a16b17b8
Noah B Johnson <noah@heartfelt.tech>
2020-02-15 23:45:28 -0600

    nav styles

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,31 +2,33 @@ import React from 'react'
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
+import './navbar.css'
 
 export const NavBar = () => {
   return (
-    <Navbar bg={'dark'} expand={'lg'} variant={'dark'} collapseOnSelect>
+    <Navbar bg={'dark'} expand={'lg'} variant={'dark'} collapseOnSelect className={'headerFont'}>
       <Navbar.Brand href="#">Noah B. Johnson</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav>
+        <Nav variant={'pills'}>
           <Nav.Link href="index.html#about">About</Nav.Link>
           <Nav.Link href="index.html#skills">Skills</Nav.Link>
           <Nav.Link href="index.html#education">Education</Nav.Link>
           <Nav.Link href="index.html#experience">Experience</Nav.Link>
           <Nav.Link href="index.html#contact">Contact</Nav.Link>
-          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="ds.html">Data Science</NavDropdown.Item>
-            <NavDropdown.Item href="photos.html">Photography</NavDropdown.Item>
+          <NavDropdown title="Projects" id="collapsible-nav-dropdown" className={'headerFont'}>
+            <NavDropdown.Item href="ds.html" className={'headerFont'}>Data Science</NavDropdown.Item>
+            {/*<NavDropdown.Item href="photos.html">Photography</NavDropdown.Item>*/}
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
       <Navbar.Collapse className="justify-content-end">
         <Nav>
-          <Nav.Link href="https://www.linkedin.com/in/noahbjohnson/"><FontAwesomeIcon icon={faLinkedin}/></Nav.Link>
-          <Nav.Link href="https://github.com/noahbjohnson"><FontAwesomeIcon icon={faGithub}/></Nav.Link>
+          <Nav.Link href="https://www.linkedin.com/in/noahbjohnson/"><FontAwesomeIcon icon={faLinkedin}
+                                                                                      size={'lg'}/></Nav.Link>
+          <Nav.Link href="https://github.com/noahbjohnson"><FontAwesomeIcon icon={faGithub} size={'lg'}/></Nav.Link>
           <Nav.Link href="https://stackoverflow.com/users/5379654/noah-b-johnson"><FontAwesomeIcon
-            icon={faStackOverflow}/></Nav.Link>
+            icon={faStackOverflow} size={'lg'}/></Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
