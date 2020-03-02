49e73473552679f92de7bcb925e2d67f456a419b
Noah B Johnson <noah@heartfelt.tech>
2020-03-02 16:59:15 -0600

    wow that was a real cluster

--- src/pages/Apps.tsx
+++ src/pages/Apps.tsx
@@ -1,11 +1,11 @@
-import React, { useContext } from 'react'
+import React, { FC, useContext } from 'react'
 import { Button, Card, Col, Container, Row } from 'react-bootstrap'
 import { PreferencesContext } from '../contexts/Preferences'
 import '../assets/styles/apps.css'
-import { myApps } from '../content/apps'
-import { Link } from 'react-router-dom'
+import { Switch, Route, Link } from 'react-router-dom'
+import { Reps } from '../content/apps/reps'
 
-export const Apps = () => {
+const DefaultView: FC = () => {
   const { darkMode } = useContext(PreferencesContext)
   return <Container className={`page ${darkMode ? 'dark' : 'light'}`} fluid>
     <header>
@@ -17,21 +17,32 @@ export const Apps = () => {
       </Row>
     </header>
     <Row>
-      {myApps.map((app, index) => {
-        return <Col lg={6} key={index}>
-          <Card className={`blog-card ${darkMode ? 'dark' : 'light'}`}>
-            <Card.Body className={`blog-body ${darkMode ? 'dark' : 'light'}`}>
-              <Card.Title className={`blog-title ${darkMode ? 'dark' : 'light'}`}>{app.name}</Card.Title>
-              <Card.Text className={`blog-text ${darkMode ? 'dark' : 'light'}`}>{app.description}</Card.Text>
-            </Card.Body>
-            <Card.Footer className={`blog-footer ${darkMode ? 'dark' : 'light'}`}>
-              <Link to={`/apps/${app.slug || ''}`}>
-                <Button variant={darkMode ? 'outline-light' : 'outline-dark'}>Open</Button>
-              </Link>
-            </Card.Footer>
-          </Card>
-        </Col>
-      })}
+      <Col lg={6}>
+        <Card className={`blog-card ${darkMode ? 'dark' : 'light'}`}>
+          <Card.Body className={`blog-body ${darkMode ? 'dark' : 'light'}`}>
+            <Card.Title className={`blog-title ${darkMode ? 'dark' : 'light'}`}>reps</Card.Title>
+            <Card.Text className={`blog-text ${darkMode ? 'dark' : 'light'}`}>Find your elected
+              representatives</Card.Text>
+          </Card.Body>
+          <Card.Footer className={`blog-footer ${darkMode ? 'dark' : 'light'}`}>
+            <Link to={'/apps/reps'}>
+            <Button variant={darkMode ? 'outline-light' : 'outline-dark'}>Open</Button>
+            </Link>
+          </Card.Footer>
+        </Card>
+      </Col>
     </Row>
   </Container>
+}
+
+export const Apps = () => {
+
+  return <Switch>
+    <Route exact path={'/apps/reps'}>
+      <Reps/>
+    </Route>
+    <Route>
+      <DefaultView/>
+    </Route>
+  </Switch>
 }
