c31247ee53174e02b64eedd389d72109b23edd4f
Noah B Johnson <njohnson@cloudtamer.io>
2020-12-22 16:10:17 -0600

    add chakra provider

--- src/app.tsx
+++ src/app.tsx
@@ -10,29 +10,32 @@ import { Blog } from './pages/blog/Blog'
 import { Apps } from './pages/apps/Apps'
 import Footer from './components/footer/footer'
 import React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function () {
   return (
-    <HashRouter>
-      <PreferencesProvider>
-        <PreferencesContext.Consumer>
-          {() => (
-            <div className="App">
-              <NavBar/>
-              <Switch>
-                <Route path='/about' component={withTracker(About)}/>
-                <Route path='/data-science'
-                       component={withTracker(() => <DataScience projects={dataScienceProjects}/>)}/>
-                <Route exact path="/blog/:slug" component={withTracker(BlogEntry)}/>
-                <Route path='/blog' component={withTracker(Blog)}/>
-                <Route path={['/apps', '/apps/**']} component={withTracker(Apps)}/>
-                <Redirect path={'**'} to={'/about'}/>
-              </Switch>
-              <Footer/>
-            </div>
-          )}
-        </PreferencesContext.Consumer>
-      </PreferencesProvider>
-    </HashRouter>
+    <ChakraProvider>
+      <HashRouter>
+        <PreferencesProvider>
+          <PreferencesContext.Consumer>
+            {() => (
+              <div className="App">
+                <NavBar/>
+                <Switch>
+                  <Route path='/about' component={withTracker(About)}/>
+                  <Route path='/data-science'
+                         component={withTracker(() => <DataScience projects={dataScienceProjects}/>)}/>
+                  <Route exact path="/blog/:slug" component={withTracker(BlogEntry)}/>
+                  <Route path='/blog' component={withTracker(Blog)}/>
+                  <Route path={['/apps', '/apps/**']} component={withTracker(Apps)}/>
+                  <Redirect path={'**'} to={'/about'}/>
+                </Switch>
+                <Footer/>
+              </div>
+            )}
+          </PreferencesContext.Consumer>
+        </PreferencesProvider>
+      </HashRouter>
+    </ChakraProvider>
   )
 }
